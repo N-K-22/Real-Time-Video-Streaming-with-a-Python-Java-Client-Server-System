976e0e7e8f8e0628988ca5f512e43fe51bd3c8f8
N-K-22 <khamarnitya@gmail.com>
2025-04-22 21:59:30 -0400

    updated to listen to UDP socket and how tohandle each received frame

--- server/communication.py
+++ server/communication.py
@@ -2,6 +2,8 @@ import socketio
 import datetime as DATE
 import asyncio
 import aiohttp
+import threading
+import socket
 #used python-socketio.readthedocs.io/en/v4/server.html to figure out whether to use coroutines and become more familiar with the library
 if __debug__: # if i want to put into debug by default, remove by compiling with -O flag
     #resoruce for this:https://www.reddit.com/r/learnpython/comments/tolt28/are_preprocessor_directives_available_in_python/
@@ -9,29 +11,43 @@ if __debug__: # if i want to put into debug by default, remove by compiling with
 else:
     server = socketio.AsyncServer(async_mode='aiohttp')  #initilizes the thread mode to be true
 
+def UDP_transfer():
+    SOCKET.settimeout(15)
+    connection_result = SOCKET.bind((IP,PORT))
+    if connection_result != 0:
+        print("Connection is not possible over UDP with the socket")
+        return
+    while True:
+        data, discard = SOCKET.recvfrom(4096) #are we able to assume this value?
+        server.emit('feedback', {"timestamp":DATE.now(), "frame_size":len(data)})
+       # my_event()
+
+
+SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 #application = socketio.ASGIApp(server)
 application = server.Application()
 server.attach(application)
 
-
-
 @server.event
 def connect(sid,environ): #socket connected
     print("Connected: ", sid)
-    return
+    #return
 
 @server.event
-def disconnect(sid): #socket disconnected
+async def disconnect(sid): #socket disconnected
     print("Disconnected: ", sid)
-    return
+    #return
 
-@server.on('feedback') #check syntax for this
-def my_event(sid,data):
+@server.on('feedback') #check syntax for this --> might cause an issue to look into
+def my_event(sid,data): 
     server.emit('feedback', {"timestamp":DATE.now(), "frame_size":len(data)}, room=sid)
-    return 
+    return "OK" , 123 #https://stackoverflow.com/questions/7872611/in-python-what-is-the-difference-between-pass-and-return
 
 if __name__ == '__main__':
     #application.run(threaded=True)
+    #getting my hands a little wet with python multithreading
+    #used this method: https://docs.python.org/3/library/threading.html
+    t1 = threading.Thread(target=UDP_transfer)
+    t1.start()
     aiohttp.web.run_app(application)
-
