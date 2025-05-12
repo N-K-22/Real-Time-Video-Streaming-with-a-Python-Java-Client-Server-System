cef5072fc9e1dafd08118d7bf5ea5b78507ba45c
N-K-22 <157038848+N-K-22@users.noreply.github.com>
2025-05-12 11:56:49 -0400

    Update communication.py

--- server/communication.py
+++ server/communication.py
@@ -17,16 +17,16 @@ if __debug__: # if i want to put into debug by default, remove by compiling with
 else:
     server = socketio.AsyncServer(async_mode='aiohttp')  #initilizes the thread mode to be true
 
-def UDP_transfer(): #set up for USP set up
+async def UDP_transfer(): #set up for USP set up
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as SOCKET:
-        SOCKET.settimeout(None)
+        SOCKET.settimeout(5)
         connection_result = SOCKET.bind((IP,PORT))
+        SOCKET.setblocking(1) #https://stackoverflow.com/questions/19570672/non-blocking-error-when-adding-timeout-to-python-server 
         while True:
-            data, discard = SOCKET.recvfrom(4096) #are we able to assume this value?
+            data, discard = await asyncio.get_event_loop().sock_recvfrom(SOCKET, 4096)
             print(data)
             server.emit('feedback', {"timestamp":DATE.now(), "frame_size":len(data)})
-        # my_event()
-
+        close_UDP(SOCKET)
 def close_UDP(s): #CLOSE SOCKET
     s.close()
 
@@ -35,29 +35,36 @@ def close_UDP(s): #CLOSE SOCKET
 @server.event
 async def connect(sid,environ): #socket connected
     print("Connected: ", sid)
-    #return
+
 
 @server.event
 async def disconnect(sid): #socket disconnected
     print("Disconnected: ", sid)
-    #return
 
-@server.event #check syntax for this --> might cause an issue to look into
+
+@server.event 
 async def my_event(sid,data): 
     server.emit('feedback', {"timestamp":DATE.now(), "frame_size":len(data)}, room=sid)
     return "OK" , 123 #https://stackoverflow.com/questions/7872611/in-python-what-is-the-difference-between-pass-and-return
 
+async def init():
 
-def main():
-     #application.run(threaded=True)
     #getting my hands a little wet with python multithreading
     #used this method: https://docs.python.org/3/library/threading.html
     #application = socketio.ASGIApp(server) #not needed for implementation
     application = web.Application() #based off of this resource: https://python-socketio.readthedocs.io/en/latest/server.html#id6
     server.attach(application)
-    t1 = threading.Thread(target=UDP_transfer, daemon=True)
-    t1.start()
-    aiohttp.web.run_app(application, port =WEB_PORT,host="0.0.0.0")
+    print("Creating Asyncio Task for Receiving Data Over UDP")
+    task = asyncio.create_task(UDP_transfer()) #runs in background
+    print("Background UDP transfer set up complete")
+    return application
 
+def main():
+    application = asyncio.run(init()) #use https://docs.python.org/3/library/asyncio.html
+    print("Starting the web application")
+    aiohttp.web.run_app(application, port =WEB_PORT,host="0.0.0.0")
+    print("Web application closed")
+
+    
 if __name__ == '__main__':
    main()
