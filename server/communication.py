484695591a006cda036f630c127202b397d9d71b
N-K-22 <157038848+N-K-22@users.noreply.github.com>
2025-05-12 15:21:15 -0400

    Update communication.py

--- server/communication.py
+++ server/communication.py
@@ -23,9 +23,9 @@ async def UDP_transfer(): #set up for USP set up
         connection_result = SOCKET.bind((IP,PORT))
         SOCKET.setblocking(1) #https://stackoverflow.com/questions/19570672/non-blocking-error-when-adding-timeout-to-python-server 
         while True:
-            data, discard = await asyncio.get_event_loop().sock_recvfrom(SOCKET, 4096)
+            data, discard = await asyncio.get_event_loop().sock_recvfrom(SOCKET, 65535)
             print(data)
-            server.emit('feedback', {"timestamp":DATE.now(), "frame_size":len(data)})
+            await server.emit("feedback", {"timestamp":DATE.now(), "frame_size":len(data)})
         close_UDP(SOCKET)
 def close_UDP(s): #CLOSE SOCKET
     s.close()
@@ -41,10 +41,16 @@ async def connect(sid,environ): #socket connected
 async def disconnect(sid): #socket disconnected
     print("Disconnected: ", sid)
 
+@server.event
+async def feedback(sid,data):
+    print(f"time stamp: {DATE.now()}, frame_size: {len(data)}")
+    server.emit("feedback", {"timestamp":DATE.now(), "frame_size":len(data)}, room=sid)
+    return "OK" , 123 #https://stackoverflow.com/questions/7872611/in-python-what-is-the-difference-between-pass-and-return
 
 @server.event 
 async def my_event(sid,data): 
-    server.emit('feedback', {"timestamp":DATE.now(), "frame_size":len(data)}, room=sid)
+    print(f"time stamp: {DATE.now()}, frame_size: {len(data)}")
+    server.emit("feedback", {"timestamp":DATE.now(), "frame_size":len(data)}, room=sid)
     return "OK" , 123 #https://stackoverflow.com/questions/7872611/in-python-what-is-the-difference-between-pass-and-return
 
 async def init():
@@ -61,10 +67,12 @@ async def init():
 
 def main():
     application = asyncio.run(init()) #use https://docs.python.org/3/library/asyncio.html
-    print("Starting the web application")
+    print("Starting the web server")
     aiohttp.web.run_app(application, port =WEB_PORT,host="0.0.0.0")
-    print("Web application closed")
+    print("Web server closed")
 
     
 if __name__ == '__main__':
    main()
+
+
